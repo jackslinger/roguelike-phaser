c6f404acca62b3b468e4f03584d4f8412a15062f
Jack Slinger <jackslinger10@gmail.com>
2017-02-26 19:07:45 +0000

    Added some basic programmer art. Made the face move around the screen.

--- assets/javascripts/game.js
+++ assets/javascripts/game.js
@@ -1,14 +1,70 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser', {});
+TILE_WIDTH = 16;
+TILE_HEIGHT = 16;
+SCREEN_WIDTH_TILES = 50;
+SCREEN_HEIGHT_TILES = 35;
+
+var game = new Phaser.Game(TILE_WIDTH * SCREEN_WIDTH_TILES, TILE_HEIGHT * SCREEN_HEIGHT_TILES, Phaser.AUTO, 'phaser', {});
 
 game_state = {
   preload: function() {
-
+    this.load.image('box', 'assets/images/box.png');
+    this.load.image('cross', 'assets/images/cross.png');
+    this.load.image('face', 'assets/images/face.png');
   },
   create: function() {
+    game.stage.backgroundColor = "#FFFFFF";
 
+    var map = [];
+    for (var col_index = 0; col_index < SCREEN_WIDTH_TILES; col_index++) {
+      var row = [];
+      for (var row_index = 0; row_index < SCREEN_HEIGHT_TILES; row_index++) {
+        row.push(null);
+      }
+      map.push(row);
+    }
+
+    map[0][0] = 'box';
+
+    for (var col_index = 0; col_index < map.length; col_index++) {
+      for (var row_index = 0; row_index < map[row_index].length; row_index++) {
+        var tile = map[col_index][row_index];
+        if (!(tile === null)) {
+          this.add.sprite(col_index * TILE_WIDTH, row_index * TILE_HEIGHT, map[col_index][row_index]);
+        }
+      }
+    }
+
+    player = this.add.sprite(16, 16, 'face');
+    keyboard = this.input.keyboard.createCursorKeys();
+    blocking = false;
   },
   update: function() {
-
+    if (keyboard.left.isDown) {
+      if (!blocking) {
+        player.x -= 16;
+        blocking = true;
+      }
+    }
+    else if (keyboard.right.isDown) {
+      if (!blocking) {
+        player.x += 16;
+        blocking = true;
+      }
+    }
+    else if (keyboard.up.isDown) {
+      if (!blocking) {
+        player.y -= 16;
+        blocking = true;
+      }
+    }
+    else if (keyboard.down.isDown) {
+      if (!blocking) {
+        player.y += 16;
+        blocking = true;
+      }
+    } else {
+      blocking = false;
+    }
   },
   render: function() {
 
